de7717741054cf0ce18604c27558c79cadf411e3
LukacTomas <lukactv@gmail.com>
2021-11-04 21:56:48 +0100

    Navigation reseted - going for mui

--- src/Components/Navigation/index.js
+++ src/Components/Navigation/index.js
@@ -1,14 +1,6 @@
 import React from "react";
 import { pages } from "./pages";
-
+s
 export const Navigation = () => {
-  return (
-    <ul className={{}}>
-      {pages.map((page) => (
-        <li key={page.hash}>
-          <a href={page.hash}>{page.title}</a>
-        </li>
-      ))}
-    </ul>
-  );
+  return <></>;
 };
